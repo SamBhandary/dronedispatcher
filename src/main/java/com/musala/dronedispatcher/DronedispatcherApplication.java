76e10c32e267be2e7f7a5bce2ebb666874e21912
Ghanashyam Bhandary <gby@logpoint.com>
2023-06-12 09:45:08 +0545

    Initial Commit Project generated from spring starter site, included web dependency

---
+++ src/main/java/com/musala/dronedispatcher/DronedispatcherApplication.java
@@ -0,0 +1,13 @@
+package com.musala.dronedispatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronedispatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronedispatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musala/dronedispatcher/DronedispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronedispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronedispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
